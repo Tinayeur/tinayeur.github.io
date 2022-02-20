1409e071d7e6da6f0ff4226f005a3ad079eb1324
Tinayeur <82331224+Tinayeur@users.noreply.github.com>
2022-02-20 15:50:08 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,23 @@
+const config = {
+    USERNAME: "Tinayeur's#9111",
+    PICTURE: "assets/img/logo.jpeg",
+
+    DESCRIPTION: "Hello everyone, I'm Tinayeur's! 18 years old, french developper orient web, I lead, develop, moderate several communities and large servers. I speak French and English.",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "assets/img/badge_bravery.png" },
+        { id: "Nitro Sub", image: "assets/img/badge_nitro.png" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/logo_c4y.png", link: "https://discord.gg/jH6wB3B6s8", name: "Coding 4 You - Owner" },
+        { id: "project-2", image: "assets/img/logo_keocloud.png", link: "https://discord.gg/ZSZwcyB6TA", name: "KeoCloud - Staff & Developer" },
+        { id: "project-3", image: "assets/img/logo_discbot.jpg", link: "https://discord.gg/5N2X3GG2jz", name: "DiscBot - Owner" },
+        // { id: "project-4", image: "assets/img/logo_hackademia.png", link: "https://discord.gg/ssrg28tvhj", name: "ProjectList - Staff & Developer" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Tinayeur" },
+        { class_name: "fab fa-opera", link: "https://discbot_discord.com"}
+    ],
+}
